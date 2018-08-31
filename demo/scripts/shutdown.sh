8fde18f44faf3d9dfad58190f7cb75319d7a28b7
sic777 <3646406@qq.com>
2018-08-31 16:32:22 +0800

    更新脚本

--- demo/scripts/shutdown.sh
+++ demo/scripts/shutdown.sh
@@ -3,17 +3,17 @@
 WK_PATH=.
 #jar name
 JAR=${project.artifactId}-${project.version}.jar
-#pid file path
-PID_FILE_PATH=${WK_PATH}/${project.artifactId}.pid
+#pid file
+PID_FILE=${WK_PATH}/${project.artifactId}.pid
 #pid
-PID=""
+PID=
 
 if [ ! -n "$WK_PATH" ];then
-    echo "please set up the work path : [WK_PATH]"
+    echo "[ERROR] please set up the work path : WK_PATH"
     exit
 fi
 
-for LINE in `cat ${PID_FILE_PATH}`
+for LINE in `cat ${PID_FILE}`
   do
    PID="$PID$LINE"
   done
@@ -21,19 +21,20 @@ for LINE in `cat ${PID_FILE_PATH}`
 G_PID=`ps -ef | grep ${JAR} |grep -v 'grep' |awk '{print $2}'`
 
 if [ ! -n "$PID" ];then
-  echo "pid not exists"
-  exit
+    echo "[ERROR] pid not exists"
 elif [ ! -n "$G_PID" ];then
-  echo "process not exists"
-  echo "">${PID_FILE_PATH}
-  exit
-elif [ "$PID" != "$G_PID" ];then
-  echo "pid error"
-  echo "">${PID_FILE_PATH}
-  exit
+    echo "[ERROR] process not exists"
 else
-#  echo "kill proceess pid:[$PID]"
   kill ${PID}
-  echo "">${PID_FILE_PATH}
-  echo "service shutdown success."
+    if [ $? -ne 0 ];then
+        echo "[ERROR] service shutdown failed."
+        exit
+    else
+        echo "kill pid:[$PID]"
+        echo "[INFO] service shutdown success."
+    fi
+fi
+
+if [ -w "$PID_FILE" ];then
+    `rm -rf ${PID_FILE}`
 fi
