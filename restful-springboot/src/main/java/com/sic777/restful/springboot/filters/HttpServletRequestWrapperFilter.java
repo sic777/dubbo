5cb02bb09d8726d267d571676b2db13f2c5d959a
sic777 <3646406@qq.com>
2019-04-20 21:27:33 +0800

    update

--- restful-springboot/src/main/java/com/sic777/restful/springboot/filters/HttpServletRequestWrapperFilter.java
+++ restful-springboot/src/main/java/com/sic777/restful/springboot/filters/HttpServletRequestWrapperFilter.java
@@ -2,13 +2,11 @@ package com.sic777.restful.springboot.filters;
 
 
 import com.sic777.common.utils.proguard.NoProguard;
-import com.sic777.restful.base.constants.HttpConstants;
 import com.sic777.restful.springboot.filters.wrapper.BodyReaderHttpServletRequestWrapper;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
@@ -26,9 +24,10 @@ public class HttpServletRequestWrapperFilter implements Filter {
         if (request instanceof HttpServletRequest) {
             HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-            // 遇到post方法才对request进行包装
+            // 遇到post、put方法才对request进行包装
             String methodType = httpRequest.getMethod();
-            if ("POST".equals(methodType.toUpperCase())) {
+            String method = methodType.toUpperCase();
+            if ("POST".equals(method) || "PUT".equals(method)) {
                 requestWrapper = new BodyReaderHttpServletRequestWrapper((HttpServletRequest) request);
             }
         }
